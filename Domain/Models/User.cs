8a21834893f15ccbcefc37c1a1b4f892c2b6e5fb
Merethe <299922@via.dk>
2022-10-25 09:28:53 +0200

    login dto

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -5,4 +5,6 @@ public class User
     public string Username { get; set; }
     public int Id { get; set; }
     public string Password { get; set; }
+    
+    public int SecurityLevel { get; set; }
 }
